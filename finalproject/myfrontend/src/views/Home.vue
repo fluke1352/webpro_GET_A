b9e80676300f4aa73ca991aa7de5fc989f524a55
fluke1352 <56115104+fluke1352@users.noreply.github.com>
2021-04-16 23:21:55 +0700

    Update Home.vue

--- finalproject/myfrontend/src/views/Home.vue
+++ finalproject/myfrontend/src/views/Home.vue
@@ -5,18 +5,105 @@
     </div>
     <div class="columns">
       <div class="slideshow-container">
-        <div class="mySlides fade">
+        <div class="fade" id="fade">
           <img
-            :src="slide[0]"
-            
+            :src="slide[indexsslide]"
             style="width: 100%; object-fit: cover"
           />
         </div>
 
-        <a class="prev" @click="indexslide--">&#10094;</a>
-        <a class="next" @click="indexslide++">&#10095;</a>
+        <a
+          class="prev"
+          @click="
+            indexslide--;
+            fade();
+          "
+          >&#10094;</a
+        >
+        <a
+          class="next"
+          @click="
+            indexslide++;
+            fade();
+          "
+          >&#10095;</a
+        >
       </div>
     </div>
+    <div class="columns mx-5 has-text-centered mb-5" >
+      <div class="column has-background-primary" style="position: relative; height: 275px;">
+        <img
+          src="https://th-test-11.slatic.net/shop/cd5cec15f2b01477c7d153cfd35e4e2a.jpeg"
+          alt=""
+          class="showimage"
+        />
+        <div class="showproduct">Speaker</div>
+      </div>
+
+      <div
+        class="column has-background-primary mx-3"
+        style="position: relative; height: 275px;"
+      >
+        <img
+          src="https://www.tuek-klongthom.com/wp-content/uploads/wpsc/product_images/2DIN-DNX4330.jpg"
+          alt=""
+          class="showimage"
+        />
+        <div class="showproduct">Radio</div>
+      </div>
+
+      <div
+        class="column has-background-primary mx-3"
+        style="position: relative; height: 275px; object-fit: cover"
+      >
+        <img
+          src="https://image.makewebeasy.net/makeweb/0/FpbNivp4p/lamina/09_lamina_POP.jpg"
+          alt=""
+          class="showimage"
+        />
+        <div class="columns">
+        <div class="showproduct">film</div></div>
+      </div>
+
+    </div>
+
+    <div class="columns mx-5 has-text-centered mb-5" >
+      <div class="column has-background-primary" style="position: relative; height: 275px;">
+        <img
+          src="https://th-live-01.slatic.net/p/69b9b7e209fd466812122bb458af45be.jpg"
+          alt=""
+          class="showimage"
+        />
+        <div class="showproduct">camere</div>
+      </div>
+
+      <div
+        class="column has-background-primary mx-3"
+        style="position: relative; height: 275px;"
+      >
+        <img
+          src="https://lh3.googleusercontent.com/proxy/3GeJAEH5YINEXeJ6iGbX4_a7P-E7e9uZVSLcwBhynma3bzzIGu8CZJiThxw_JoHw6TGNh5ZzQbAquO7oRmOBX3f341UILo3fg8FmBR8"
+          alt=""
+          class="showimage"
+        />
+        <div class="showproduct">sensor</div>
+      </div>
+
+      <div
+        class="column has-background-primary mx-3"
+        style="position: relative; height: 275px; object-fit: cover"
+      >
+        <img
+          src="https://cdn.discordapp.com/attachments/603631846525501460/832636104422719499/unknown.png"
+          alt=""
+          class="showimage"
+        />
+        <div class="columns">
+        <div class="showproduct">All</div></div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
@@ -30,8 +117,32 @@ export default {
       // console.log(this.data);
     });
   },
+  computed: {
+    indexsslide() {
+      let a = 0;
+      if (this.indexslide < 0) {
+        a = this.slide.length - 1;
+      } else if (this.slide.length == this.indexslide) {
+        a = 0;
+      } else if (this.slide.length > this.indexslide && this.indexslide != 0) {
+        a = this.indexslide;
+      }
+      this.indexvalue(a);
+      return a;
+    },
+  },
   methods: {
     fetchdb() {},
+    indexvalue(a) {
+      this.indexslide = a;
+    },
+    fade() {
+      console.log("reani");
+      var element = document.getElementById("fade");
+      element.classList.remove("fade");
+      void element.offsetWidth; 
+      element.classList.add("fade");
+    },
   },
   data() {
     return {
@@ -48,6 +159,18 @@ export default {
 </script>
 
 <style scoped>
+.showproduct {
+  position: absolute;
+  padding: 10px;
+  margin-top: -50%;
+  font-size: 6em;
+}
+
+.showimage {
+  height: 100%;
+  object-fit: cover
+}
+
 .slideshow-container {
   width: 100%;
   position: relative;
@@ -89,15 +212,6 @@ export default {
   -webkit-animation-duration: 1.5s;
   animation-name: fade;
   animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {
-    opacity: 0.4;
-  }
-  to {
-    opacity: 1;
-  }
 }
 
 @keyframes fade {

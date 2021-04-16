f08e8620244e1730db9d4fbc83f102f6a5646552
fluke1352 <56115104+fluke1352@users.noreply.github.com>
2021-04-16 14:03:13 +0700

    Update config.js

--- finalproject/backend/config.js
+++ finalproject/backend/config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Fluke010444',
+  password: 'password',
   database: '999auto',
   waitForConnections: true,
   connectionLimit: 10,

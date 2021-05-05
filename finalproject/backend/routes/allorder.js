8f9160068bf24121d39e74eb799dcaa900d22662
bankjirayuth <b_jirayuth@hotmail.com>
2021-05-05 23:43:42 +0700

    allorder back and front

--- finalproject/backend/routes/allorder.js
+++ finalproject/backend/routes/allorder.js
@@ -10,14 +10,24 @@ router.post("/allorder", async (req, res, next) => {
     const conn = await pool.getConnection();
     await conn.beginTransaction();
     try {
-        let [info, _] = await conn.query(
-            "select * from 999auto.order o join user u on(u.user_id = o.user_user_id)"
-        );
-        
-        res.json({
-            message: info
-
-        });
+        if (req.body.status) {
+            let sordby = req.body.status
+            console.log(sordby);
+            let [info, _] = await conn.query(
+                "select * from 999auto.order o join user u on(u.user_id = o.user_user_id) order by " + sordby
+            );
+            res.json({
+                message: info
+            });
+        }
+        else {
+            let [info,] = await conn.query(
+                "select * from 999auto.order o join user u on(u.user_id = o.user_user_id)"
+            );
+            res.json({
+                message: info
+            });
+        }
         conn.commit();
         conn.release();
 
@@ -32,7 +42,7 @@ router.post("/datailorder", async (req, res, next) => {
     await conn.beginTransaction();
     try {
         let [product, a] = await conn.query("SELECT * FROM 999auto.order_item join product on(product_product_id = product_id);")
-        
+
         res.json({
             message: product
 
--- finalproject/myfrontend/src/views/allorder.vue
+++ finalproject/myfrontend/src/views/allorder.vue
@@ -1,39 +1,64 @@
 <template>
-  <div class="container">
-    <div
-      class="columns my-5 has-background-danger"
-      v-for="(order, index) in orders"
-      :key="index"
-    >
-      <!-- <div class="column">s</div> -->
-      <img
-        :src="imagePath(order.user_image)"
-        class="my-4 mx-4"
-        style="width: 120px; object-fit: cover"
-        alt=""
-      />
-      <div class="column">
-        <p class="is-size-4">ORDER ID : {{ order.order_id }}</p>
-        <p class="is-size-5">
-          CUSTOMER NAME : {{ order.user_fname + " " + order.user_lname }}
-        </p>
-        <p class="is-size-6">DELIVERY DATE : {{ order.delivery_date }}</p>
-        <p class="is-size-6">ORDER DATE : {{ order.order_date }}</p>
-        <p class="is-size-6">TOTAL PRICE : {{ order.price_of_all_item }} ฿</p>
-        <p class="is-size-6">ORDER LIST :</p>
+  <div class="bg">
+    <div class="container">
+      <button class="button mx-1 has-background-warning has-text-black" @click="sortby('order_id')">SOTR BY ORDER ID</button>
+    <button class="button mx-1 has-background-warning has-text-black" @click="sortby('delivery_date')">SOTR BY DELIVERY DATE</button>
+
+      <!-- <template v-for="(order, index) in orders" :key="index"> -->
+      <div
+        class="columns mb-3 has-text-warning"
+        style="background: #363636"
+        v-for="(order, index) in orders" :key="index">
         <div
-          class="is-size-6"
-          v-for="(product, index) in products"
-          :key="index"
+          v-if="index < valmax && index >= valmin"
+          :class="{ 'is-hidden': !(index < valmax && index >= valmin) }"
         >
-          <div v-if="product.order_order_id == order.order_id">
-            {{ product.product_name }}
+          <img
+            :src="imagePath(order.user_image)"
+            class="columns my-4 mx-4 has-background-has-background-dark"
+            style="width: 120px; object-fit: cover"
+            alt=""
+          />
+        </div>
+        <div
+          v-if="index < valmax && index >= valmin"
+          :class="{ 'is-hidden': !(index < valmax && index >= valmin) }"
+        >
+          <div class="column">
+            <p class="is-size-4">ORDER ID : {{ order.order_id }}</p>
+            <p class="is-size-5">
+              CUSTOMER NAME : {{ order.user_fname + " " + order.user_lname }}
+            </p>
+            <p class="is-size-6">DELIVERY DATE : {{ order.delivery_date }}</p>
+            <p class="is-size-6">ORDER DATE : {{ order.order_date }}</p>
+            <p class="is-size-6">
+              TOTAL PRICE : {{ order.price_of_all_item }} ฿
+            </p>
+            <p class="is-size-6">ORDER LIST :</p>
+            <div
+              class="is-size-6"
+              v-for="(product, index) in products"
+              :key="index"
+            >
+              <div v-if="product.order_order_id == order.order_id">
+                {{ product.product_name }}
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      <!-- </template> -->
+      <div class="columns  is-centered  mt-4">
+          <button class="column is-1 " @click="valmax = 5; valmin = 0; page = 1;">&lt;&lt;</button>
+          <button class="column is-1 ml-3" @click="valmax -= 5; valmin -= 5; page--;">&lt;</button>
+         <div class="column is-8 has-text-warning"><p  style="text-align:center; width:100%;" class="is-size-6"><b>{{pages + '/' + maxpage}}</b></p></div>
+          <button class="column is-1 mr-3" @click="valmax += 5; valmin += 5; page++;">&gt;</button>
+          <button class="column is-1 " @click="setpage( 5*maxpage, 5*(maxpage-1), maxpage)">&gt;&gt;</button>
+        </div>
     </div>
   </div>
 </template>
+
 
 <script>
 import axios from "axios";
@@ -42,10 +67,12 @@ export default {
   created() {
     axios.post("http://localhost:3000/allorder").then((res) => {
       this.orders = res.data.message;
+      this.maxpage = Math.ceil(this.orders.length / 5);
     });
     axios.post("http://localhost:3000/datailorder").then((res) => {
       this.products = res.data.message;
     });
+    console.log(this.maxpage);
   },
   methods: {
     imagePath(file_path) {
@@ -55,11 +82,47 @@ export default {
         return "https://bulma.io/images/placeholders/640x360.png";
       }
     },
+    setpage(max, min, page) {
+      this.valmax = max;
+      this.valmin = min;
+      this.page = page;
+    },
+    sortby(status) {
+      axios
+        .post("http://localhost:3000/allorder", { status: status })
+        .then((res) => {
+          this.orders = res.data.message;
+          this.maxpage = Math.ceil(this.orders.length / 5);
+        });
+    },
+  },
+  computed: {
+    pages() {
+      let page = 1;
+      let maxpage = this.maxpage;
+      // console.log(maxpage);
+      if (this.page < 0 || this.valmin < 0) {
+        // console.log("<<<< 00");
+        this.setpage(5, 0, 1);
+        page = 1;
+      } else if (this.page > this.maxpage && this.valmax > this.orders.length) {
+        page = this.maxpage;
+        this.setpage(5 * maxpage, 5 * (maxpage - 1), maxpage);
+      } else {
+        page = this.page;
+      }
+
+      return page;
+    },
   },
   data() {
     return {
       orders: [],
       products: [],
+      valmax: 5,
+      valmin: 0,
+      page: 1,
+      maxpage: null,
     };
   },
 };
@@ -68,7 +131,7 @@ export default {
 <style scoped>
 .bg {
   /* background-image: url("../assets/order.jpg"); */
-  background-image: url(https://images.pexels.com/photos/3039049/pexels-photo-3039049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260);
+  background-image: url(https://images.pexels.com/photos/4488652/pexels-photo-4488652.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260);
   height: 100%;
 
   background-position: center;
@@ -81,9 +144,11 @@ html {
 }
 .card {
   opacity: 98%;
-  transition: 0.5s;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.5);
+  transition: box-shadow 0.7s;
 }
 .card:hover {
   opacity: 100%;
+  box-shadow: 8px 8px 18px 0px #ffdd57;
 }
 </style>

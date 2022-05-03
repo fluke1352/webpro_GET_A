2bb1e32e1bcd6bd303767c80e2b208d9434ba3e3
phawit-s <56498877+phawit-s@users.noreply.github.com>
2022-05-03 14:54:34 +0700

    update: config.js

--- finalproject/backend/config.js
+++ finalproject/backend/config.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
+  host: 'auto-db.cclk4zrxortg.ap-southeast-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'xitgmLwmp12q',
   database: '999auto',
   waitForConnections: true,
   connectionLimit: 10,
